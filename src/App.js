5ff4af372fdccd96fc1ccd6d0ac913c31b117a09
Aviral <aviral.saxena.11.70@gmail.com>
2023-07-04 22:50:10 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+        const [data, setData] = useState({
+                fname: "",
+                lname: "",
+                email: "",
+                number: "",
+        });
+
+        const inputdata = (e) => {
+                const name = e.target.name;
+                const value = e.target.value;
+                // console.log(name, value);
+
+                setData({
+                        ...data,
+                        [name]: value,
+                });
+        };
+
+        const submitform = (e) => {
+                e.preventDefault();
+        };
+
+        return (
+                <>
+                        <div className="container">
+                                <form onSubmit={submitform}>
+                                        <h1>
+                                                Hello {data.fname} {data.lname}
+                                        </h1>
+                                        
+                                        <h3>{data.email}</h3>
+                                        
+                                        <h3>{data.number}</h3>
+                                        <input
+                                                type="text"
+                                                name="fname"
+                                                placeholder="Enter your name"
+                                                onChange={inputdata}
+                                        />
+                                        <br></br>
+
+                                        <input
+                                                type="text"
+                                                name="lname"
+                                                placeholder="Enter your Last name"
+                                                onChange={inputdata}
+                                        />
+                                        <br></br>
+
+                                        <input
+                                                type="email"
+                                                name="email"
+                                                placeholder="Enter your email"
+                                                onChange={inputdata}
+                                        />
+                                        <br></br>
+
+                                        <input
+                                                type="tel"
+                                                name="number"
+                                                placeholder="Enter your mobile number"
+                                                onChange={inputdata}
+                                        />
+                                        <br></br>
+
+                                        <button type="submit">Submit Me</button>
+                                </form>
+                        </div>
+                </>
+        );
+};
 
 export default App;
